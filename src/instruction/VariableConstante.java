0af975b434168ad0e9a46e3b46853e374b1d0637
Adrien <adry.azz@laposte.net>
2012-02-26 12:29:47 +0100

    Ajout des constructeurs à VariableConstante et VariableModifiable

--- src/instruction/VariableConstante.java
+++ src/instruction/VariableConstante.java
@@ -2,6 +2,9 @@ package instruction;
 
 public abstract class VariableConstante extends Variable{
 
+	public VariableConstante(TypeVariable type, String nom,  String valeur){
+		super(type, nom, valeur);
+	}
 	@Override
 	public boolean isConstante() {
 		return true;
--- src/instruction/VariableModifiable.java
+++ src/instruction/VariableModifiable.java
@@ -2,6 +2,9 @@ package instruction;
 
 public abstract class VariableModifiable extends Variable{
 
+	public VariableModifiable(TypeVariable type, String nom,  String valeur){
+		super(type, nom, valeur);
+	}
 	@Override
 	public boolean isConstante() {
 		return false;
