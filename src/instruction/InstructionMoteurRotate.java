a46fca9be4f6da6369d70520772bf4163273f106
bastien <m1002@ic2s120-12.(none)>
2012-03-02 16:40:29 +0100

    changement visiteur , delete heureActuelle

---
+++ src/instruction/InstructionMoteurRotate.java
@@ -0,0 +1,19 @@
+package instruction;
+
+import traduction.VisiteurTraduction;
+
+public class InstructionMoteurRotate extends InstructionMoteurCmd{
+
+	@Override
+	public Categorie getCategorie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void accepte(VisiteurTraduction visiteur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
