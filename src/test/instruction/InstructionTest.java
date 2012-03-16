a9905b09796658cd39aec2ac6d76019626135740
bastien <m1002@ic2s120-16.(none)>
2012-03-16 15:41:51 +0100

    Changement du InstructionTest

--- src/test/instruction/InstructionTest.java
+++ src/test/instruction/InstructionTest.java
@@ -2,9 +2,59 @@ package test.instruction;
 
 import instruction.*;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+
+
+
+/**
+ * @author m1022
+ *
+ */
 public class InstructionTest {
-	public static void test()
-	{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	//TODO automatiser
+	@Test
+	public void testComposeInstruction() throws Exception{
 		//System.out.printf("chat");
 		
 		InstructionStructure n1 = new InstructionTache();
@@ -15,14 +65,7 @@ public class InstructionTest {
 		n1.inserer(1,n3);
 		n1.inserer(2,n4);
 		System.out.println(n1);
-		
-		
-		
-	}
-	
-	public static void main(String [] args)
-	{
-		InstructionTest.test();
-	}
+
+	};
 
 }
