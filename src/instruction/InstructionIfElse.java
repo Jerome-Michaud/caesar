41a52d69d7f299daf5ef45994ac1cf94acd62b3f
Adrien <adry.azz@laposte.net>
2012-03-21 15:36:29 +0100

    Suppresssion d'un import inutile. Correction des commentaires Javadoc.

--- src/instruction/InstructionIfElse.java
+++ src/instruction/InstructionIfElse.java
@@ -1,20 +1,17 @@
 package instruction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import traduction.VisiteurTraduction;
 
 public class InstructionIfElse extends InstructionConditionelle {
 
-
 	private InstructionIf membreIf = new InstructionIf();
 	
 	public InstructionIfElse() {
 		super(null);
-	
-		
 	}
+	
 	public InstructionIfElse(Condition cond) {
 		super(null);
 		getMembreIf().setCondition(cond);
@@ -27,50 +24,45 @@ public class InstructionIfElse extends InstructionConditionelle {
 	public void setCondition(Condition cond){
 		getMembreIf().setCondition(cond);
 	}
-	
-	
+		
 	public InstructionIf getMembreIf()
 	{
 		return membreIf;
 	}
 	
-	
-	
 	@Override
 	public void accepte(VisiteurTraduction visiteur) {
 		visiteur.visiter(this);
 	}
-	
-	
 
 	/**
-	 * inserer sur if
+	 * Insère une instruction dans le corps de if.
 	 */
 	public void inserer(int position, Instruction inst){
 		membreIf.inserer(position,inst);
 	}
+	
 	/**
-	 * inserer sur if
+	 * Insère une instruction dans le corps de if.
 	 */
 	public void insererDebut(Instruction inst){
 		membreIf.insererDebut(inst);
 	}
+	
 	/**
-	 * inserer sur if
+	 * Insère une instruction dans le corps de if.
 	 */
 	public void insererFin(Instruction inst){
 		membreIf.insererFin(inst);
 	}
+	
 	/**
-	 * get If enfants
+	 * Donne les instructions du corps de if.
+	 * @return la list des instructions du corps de if.
 	 */
 	public List<Instruction> getEnfants() {
 		return membreIf.getEnfants();
 	}
-	
-	
-	
-	
 	
 	public void insererElse(int position, Instruction inst){
 		super.inserer(position,inst);
