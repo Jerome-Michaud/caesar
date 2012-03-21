53ffc14961458397a9404ecb322cbd576e2cbe41
Adrien <adry.azz@laposte.net>
2012-03-21 15:09:19 +0100

    Commentaires Javadoc de Condition

--- src/instruction/Condition.java
+++ src/instruction/Condition.java
@@ -2,8 +2,19 @@ package instruction;
 
 import traduction.VisiteurTraduction;
 
+/**
+ * Expression logique.
+ * @author Adrien DUROY, Bastien AUBRY, Ivan MELNYCHENKO
+ */
 public class Condition extends ExpressionComplexe {
 
+	/**
+	 * Crée une condition.
+	 * @param operation l'opérateur logique à utiliser
+	 * @param membreDroit le membre gaughe de la condition
+	 * @param membreGauche le membre droit de la condition
+	 * @throws Exception Si l'opérateur fourni n'est pas un opérateur logique.
+	 */
 	public Condition(Operateur operation, Expression membreDroit, Expression membreGauche) throws Exception
 	{
 		super(operation, membreDroit, membreGauche);
@@ -11,6 +22,11 @@ public class Condition extends ExpressionComplexe {
 			throw new Exception("Opérateur non logique dans Condition.");
 	}
 
+	/**
+	 * Crée une condition.
+	 * @param operateur l'opérateur logique à utiliser
+	 * @throws Exception Si l'opérateur fourni n'est pas un opérateur logique.
+	 */
 	public Condition(Operateur operateur) throws Exception {
 		super(operateur);
 		if(!Operateur.isLogique(operateur))
