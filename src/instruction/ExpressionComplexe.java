5facd1fcaae0f1434b07305725f600ebc0a1885d
Adrien <adry.azz@laposte.net>
2012-03-21 15:27:56 +0100

    Commentaires Javadoc de Expression et ExpressionComplexe. Suppression de la déclaration de toString dans l'interface Expression. Réordonnancement de méthodes d'ExpressionComplexe.

--- src/instruction/ExpressionComplexe.java
+++ src/instruction/ExpressionComplexe.java
@@ -1,15 +1,29 @@
 package instruction;
-
+/**
+ * Expression composée de deux autres expressions
+ * avec lesquelles effectuer une opération.
+ * Utilise le design pattern Composite.
+ * @author Adrien DUROY, Bastien AUBRY, Ivan MELNYCHENKO
+ */
 public abstract class ExpressionComplexe implements Expression {
-
-	public ExpressionComplexe() {
-		super();
-	}
 
 	private Expression membreDroit;
 	private Expression membreGauche;
 	private Operateur operateur;
+	
+	/**
+	 * Crée une expression vide, le contenu devra être fourni plus tard.
+	 */
+	public ExpressionComplexe() {
+		super();
+	}
 
+	/**
+	 * Crée une expression complexe.
+	 * @param operation l'opérateur de l'expression
+	 * @param membreGauche l'expression à gauche de l'opérateur
+	 * @param membreDroit l'expression à droite de l'opérateur
+	 */
 	public ExpressionComplexe(Operateur operation, Expression membreGauche,
 			Expression membreDroit) {
 		this.operateur = operation;
@@ -17,15 +31,18 @@ public abstract class ExpressionComplexe implements Expression {
 		this.membreDroit = membreDroit;
 	}
 
+	/**
+	 * Crée une expression complexe dont seul l'opérateur est défini,
+	 * les membres devront être fournis plus tard.
+	 * @param operation l'opérateur de l'expression
+	 */
 	public ExpressionComplexe(Operateur operation) {
 		this.operateur =  operation;
 	}
 
 	/**
 	 * Modifie le membre droit.
-	 * 
-	 * @param expression
-	 *            l'expression à mettre en membre droit
+	 * @param expression l'expression à mettre en membre droit
 	 */
 	public void setMembreDroit(Expression expression) {
 		membreDroit = expression;
@@ -33,22 +50,32 @@ public abstract class ExpressionComplexe implements Expression {
 
 	/**
 	 * Modifie le membre gauche.
-	 * 
-	 * @param expression
-	 *            l'expression à mettre en membre gauche
+	 * @param expression l'expression à mettre en membre gauche
 	 */
 	public void setMembreGauche(Expression expression) {
 		membreGauche = expression;
 	}
 	
+	/**
+	 * Accède au membre gauche.
+	 * @return le membre gauche.
+	 */
 	public Expression getMembreGauche() {
 		return this.membreGauche;
 	}
 	
+	/**
+	 * Accède au membre droit.
+	 * @return le membre droit.
+	 */
 	public Expression getMembreDroit() {
 		return this.membreDroit;
 	}
 	
+	/**
+	 * Accède à l'opérateur.
+	 * @return l'opérateur.
+	 */
 	public Operateur getOperateur() {
 		return this.operateur;
 	}
@@ -57,5 +84,4 @@ public abstract class ExpressionComplexe implements Expression {
 	public String toString() {
 		return "(" + membreGauche + operateur + membreDroit + ")";
 	}
-
 }
