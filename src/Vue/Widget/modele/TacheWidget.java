732c5ab180331f2500360b7dff99de135f4306d3
Vincent <vincent.besnard@gmail.com>
2012-03-26 10:59:13 +0200

    fonctions redimensionnement composants Tache et While

--- src/Vue/Widget/modele/TacheWidget.java
+++ src/Vue/Widget/modele/TacheWidget.java
@@ -22,29 +22,40 @@ public class TacheWidget extends ModeleWidget{
 			this.setForme(new Polygon(this.getTabX(), this.getTabY(), this.getTabX().length));
 		}
 
-	@Override
-	public void decalageXout(int x) {
-		
-		
-	}
+	public void decalageXout(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] + a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
+    }
 
-	@Override
-	public void decalageXin(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void decalageXin(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] - a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
+    }
 
-	@Override
-	public void decalageYout(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void decalageYout(int b) {
+        int i;
+        for (i = 8; i < 16; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] + b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
+    }
 
-	@Override
-	public void decalageYin(int x) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    public void decalageYin(int b) {
+        int i;
+        for (i = 8; i < 16; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] - b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
+    }
 
 }
--- src/Vue/Widget/modele/WhileWidget.java
+++ src/Vue/Widget/modele/WhileWidget.java
@@ -23,31 +23,47 @@ public class WhileWidget extends ModeleWidget {
 		}
 
 
-	@Override
-	public void decalageXout(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void decalageXout(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] + a;
+        }
+        for (i = 22; i < 26; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] + a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
+    }
 
+    public void decalageXin(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] - a;
+        }
+        for (i = 22; i < 26; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] - a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
+    }
 
-	@Override
-	public void decalageXin(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void decalageYout(int b) {
+        int i;
+        for (i = 16; i < this.getTabY().length; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] + b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
+    }
 
+    public void decalageYin(int b) {
+        int i;
+        for (i = 16; i < this.getTabY().length; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] - b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
+    }
 
-	@Override
-	public void decalageYout(int x) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void decalageYin(int x) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
