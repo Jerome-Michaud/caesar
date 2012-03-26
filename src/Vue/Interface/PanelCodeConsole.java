7f244015cff8b71247c0e6cd33fe5877958b47b2
Adrien <adry.azz@laposte.net>
2012-03-26 14:49:38 +0200

    Ajout d'une classe lançant la traduction à partir de l'arborescence.

--- src/Vue/Interface/PanelCodeConsole.java
+++ src/Vue/Interface/PanelCodeConsole.java
@@ -1,8 +1,21 @@
 package Vue.Interface;
 
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+
+import traduction.VisiteurNXC;
+import traduction.VisiteurTraduction;
+import Vue.Tools.ArborescenceTools;
+import Vue.Widget.Widget;
+import Vue.Widget.modele.ModeleWidget;
+import Vue.Widget.modele.TacheWidget;
+import Vue.controller.LanceurTraduction;
 
 public class PanelCodeConsole extends JPanel {
 
@@ -11,6 +24,8 @@ public class PanelCodeConsole extends JPanel {
 	private PanelCodeConsole() {
 		this.setMinimumSize(new Dimension(100, 600));
 		this.setBorder(BorderFactory.createTitledBorder("Aperçu code console"));
+		
+		this.addMouseListener(LanceurTraduction.getInstance());
 	}
 
 	public static PanelCodeConsole getInstance() {
