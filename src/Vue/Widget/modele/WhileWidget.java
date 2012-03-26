ce0e920b6f7a6c73d46d5bd5b460190ac23f1e33
Vincent <vincent.besnard@gmail.com>
2012-03-26 17:06:59 +0200

    methodes de redimensionnnement If, Instruction, While

--- src/Vue/Widget/modele/WhileWidget.java
+++ src/Vue/Widget/modele/WhileWidget.java
@@ -22,24 +22,46 @@ public class WhileWidget extends ModeleWidget implements Serializable{
         this.zonesAccroches.add(Variables.ZONE_ACCROCHE_PAR_DEFAULT);
     }
 
-    @Override
-    public void decalageXout(int x) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void decalageXout(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] + a;
+        }
+        for (i = 22; i < 26; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] + a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
     }
 
-    @Override
-    public void decalageXin(int x) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void decalageXin(int a) {
+        int i;
+        for (i = 6; i < 10; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] - a;
+        }
+        for (i = 22; i < 26; i++) {
+            this.getForme().xpoints[i] = this.getForme().xpoints[i] - a;
+        }
+        this.setForme(this.getForme());
+        this.setTailleX();
     }
 
-    @Override
-    public void decalageYout(int x) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void decalageYout(int b) {
+        int i;
+        for (i = 16; i < tabY.length; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] + b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
     }
 
-    @Override
-    public void decalageYin(int x) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void decalageYin(int b) {
+        int i;
+        for (i = 16; i < tabY.length; i++) {
+            this.getForme().ypoints[i] = this.getForme().ypoints[i] - b;
+        }
+        this.setForme(this.getForme());
+        this.setTailleY();
     }
 
 }
