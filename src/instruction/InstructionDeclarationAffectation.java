17e8531b87b062c995ec42c71d3aca0402014fdc
Adrien <adry.azz@laposte.net>
2012-03-31 11:59:28 +0200

    Réorganisation du code et commentaire javadoc de InstructionDeclarationAffectation.

--- src/instruction/InstructionDeclarationAffectation.java
+++ src/instruction/InstructionDeclarationAffectation.java
@@ -2,38 +2,56 @@ package instruction;
 
 import traduction.VisiteurTraduction;
 
+/**
+ * Déclaration d'une variable avec initialisation en même temps.
+ * @author Adrien DUROY, Bastien AUBRY
+ */
 public class InstructionDeclarationAffectation extends InstructionDeclaration {
 	
-
 	private Expression membreDroit ;
 	
+	/**
+	 * Créé une déclaration avec initialisation sans variable et expression.
+	 * Ces dernières ont pour valeur null.
+	 */
 	public InstructionDeclarationAffectation() {
 		super();
 	}
 
+	/**
+	 * Modifie la variable à déclarer et l'expression pour l'initialiser.
+	 * @param variable
+	 * @param expression
+	 * @deprecated
+	 */
 	public void setMembre( VariableModifiable variable, Expression expression ){
 		super.setMembre(variable);
 		this.membreDroit = expression;
 	}
 	
-	public String toString()
-	{
-
-		return getMembreGauche().getType() +" "+ getMembreGauche().getNom() +" = "+ membreDroit.toString() ;
-	}
-	public Expression getMembreDroit()
-	{
+	/**
+	 * Accède à l'expression d'initialisation de la variable à déclarer.
+	 * @return l'expression
+	 */
+	public Expression getMembreDroit() {
 		return membreDroit;
-
 	}
+	
+	/**
+	 * Modifie l'expression d'initialisation
+	 * @param expression
+	 */
+	public void setMembreDroit(Expression expression) {
+		membreDroit = expression;
+	}
+	
 	@Override
 	public void accepte(VisiteurTraduction visiteur) {
 		visiteur.visiter(this);
 	}
-
-	public void setMembreDroit(Expression expression) {
-		membreDroit = expression;
-		
+	
+	@Override
+	public String toString() {
+		return getMembreGauche().getType() +" "+ getMembreGauche().getNom() +" = "+ membreDroit.toString() ;
 	}
-
 }
