208ab6c99a70099cf8490fad76624259b0272b9a
Adrien <adry.azz@laposte.net>
2012-04-05 17:58:37 +0200

    InstructionMoteurCmd : - Suppression des lignes inutiles. + Commentaires Javadoc.

--- src/instruction/InstructionMoteurCmd.java
+++ src/instruction/InstructionMoteurCmd.java
@@ -1,33 +1,41 @@
 package instruction;
-
-import traduction.VisiteurTraduction;
-
+/**
+ * Instruction pour un moteur.
+ * @author Adrien DUROY, Bastien AUBRY
+ */
 public abstract class InstructionMoteurCmd implements Instruction {
 
 	private Moteur moteur;
 
+	/**
+	 * Crée une instruction un moteur non initialisée. Le moteur à pour valeur null.
+	 */
 	public InstructionMoteurCmd() {
 		super();
 		moteur = null;
 	}
 
+	/**
+	 * Crée une instruction pour un moteur.
+	 * @param moteur le moteur concerné
+	 */
 	public InstructionMoteurCmd(Moteur moteur){
 		this.moteur = moteur;
 	}
 	
-	public abstract Categorie getCategorie() ;
-	
-	
-	public abstract void accepte(VisiteurTraduction visiteur) ;
-	
+	/**
+	 * Modifie le moteur concerné par l'instruction.
+	 * @param moteur le nouveau moteur
+	 */
 	public void setMoteur(Moteur moteur){
-
 			this.moteur = moteur;
 	}
+	
+	/**
+	 * Accède au moteur concerné par l'instruction.
+	 * @return le moteur
+	 */
 	public Moteur getMoteur(){
 		return moteur;
 	}
-	
-	
-
 }
