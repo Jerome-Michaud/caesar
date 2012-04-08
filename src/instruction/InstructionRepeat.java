e5bb2f04dac5cbdf0652f51c28ecf8b2ab98747c
Adrien <adry.azz@laposte.net>
2012-04-08 12:02:03 +0200

    Commentaires Javadoc de InstructionRepeat.

--- src/instruction/InstructionRepeat.java
+++ src/instruction/InstructionRepeat.java
@@ -2,23 +2,43 @@ package instruction;
 
 import traduction.VisiteurTraduction;
 
+/**
+ * Boucle de répétition.
+ * @author Adrien DUROY, Bastien AUBRY, Ivan MELNYCHENKO
+ */
 public class InstructionRepeat extends InstructionInconditionelle{
 	
 	private Expression expression;
 	
+	/**
+	 * Crée une boucle de répétition vide sans précision du nombre de répétion.
+	 * L'expression donnant ce nombre vaut null.
+	 */
 	public InstructionRepeat() {
 		super();
 	}
 
+	/**
+	 * Crée une boucle de répétition vide.
+	 * @param expr l'expression donnant le nombre de répétition
+	 */
 	public InstructionRepeat(Expression expr) {
 		super();
 		this.expression = expr;
 	}
 
+	/**
+	 * Accède à l'expression donnant le nombre de répétition.
+	 * @return l'expression.
+	 */
 	public Expression getExpression() {
 		return expression;
 	}
 
+	/**
+	 * Modifie l'expression donnant le nombre de répétition.
+	 * @param expr la nouvelle expression
+	 */
 	public void setExpression(Expression expr) {
 		this.expression = expr;
 	}
