a3597dc53a2ae7727122beb7275cd901672470d1
Adrien <adry.azz@laposte.net>
2012-04-09 19:33:32 +0200

    Commentaires Javadoc de InstructionTache.

--- src/instruction/InstructionTache.java
+++ src/instruction/InstructionTache.java
@@ -1,29 +1,45 @@
 package instruction;
 
 import traduction.VisiteurTraduction;
-
+/**
+ * Tâche pour un programme d'un robot NXT.
+ * @author Adrien DUROY, Bastien AUBRY, Ivan MELNYCHENKO
+ */
 public class InstructionTache extends InstructionInconditionelle {
 
 	private String nom;
-		
+	
+	/**
+	 * Crée une tâche qui aura pour nom : "main".
+	 */
+	public InstructionTache() {
+		this.nom = "main";
+	}
+	
+	/**
+	 * Crée une tâche.
+	 * @param nom le nom de la tâche
+	 */
+	public InstructionTache(String nom) {
+		this.nom = nom;
+	}
+	
+	/**
+	 * Accède au nom de la tâche.
+	 * @return le nom.
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Modifie le nom de la tâche.
+	 * @param nom le nouveau nom
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-	public InstructionTache()
-	{
-		//TODO nom de tache par defaut
-		this.nom = "main";
-	}
 	
-	public InstructionTache ( String nom)
-	{
-		this.nom = nom;
-	}
 	@Override
 	public Categorie getCategorie() {
 		return Categorie.CONTROL;
@@ -44,5 +60,4 @@ public class InstructionTache extends InstructionInconditionelle {
 		res+="}";
 		return res;
 	}
-
 }
