e2f304d4b070e9128d5a6e3f225e208e2068ff21
Adrien <adry.azz@laposte.net>
2012-04-09 19:37:16 +0200

    Écriture du corps de getCategorie() de InstructionTempsCourant. Commentaires Javadoc de la même classe.

--- src/instruction/InstructionTempsCourant.java
+++ src/instruction/InstructionTempsCourant.java
@@ -1,19 +1,19 @@
 package instruction;
 
 import traduction.VisiteurTraduction;
-
+/**
+ * Instruction donnant le temps écoulé depuis le lancement du programme.
+ * @author Adrien DUROY, Bastien AUBRY, Ivan MELNYCHENKO
+ */
 public class InstructionTempsCourant implements Instruction{
 
 	@Override
 	public Categorie getCategorie() {
-		// TODO getCategorie() pour InstructionTempsCourant
-		return null;
+		return Categorie.TEMPS;
 	}
 
 	@Override
 	public void accepte(VisiteurTraduction visiteur) {
 		visiteur.visiter(this);
-		
 	}
-
 }
