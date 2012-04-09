0e240625f6f52108feac2708aab81ea8816e9629
Adrien <adry.azz@laposte.net>
2012-04-09 20:00:00 +0200

    Correction des commentaires Javadoc de VariableCapteur. Suppression de lignes inutiles.

--- src/instruction/VariableCapteur.java
+++ src/instruction/VariableCapteur.java
@@ -1,46 +1,49 @@
 package instruction;
 
 import traduction.VisiteurTraduction;
-
+/**
+ * Variable constante représentant un capteur.
+ * @author Adrien DUROY, Bastien AUBRY
+ */
 public class VariableCapteur extends VariableConstante {
-	/**
-	 * Crée une variable constante pour capteur.
-	 * @param type Le type de la variable
-	 * @param nom Le nom de la variable
-	 * @param valeur La valeur initiale de la variable
-	 */
-	
 	private CapteurSlot capteurSlot ;
 	
+	/**
+	 * Crée une variable constante pour le capteur du slot A.
+	 */
 	public VariableCapteur() {
 		super(TypeVariable.INT, "", CapteurSlot.A.toString());
 		capteurSlot = CapteurSlot.A;
-		
 	}
+	
+	/**
+	 * Crée une variable constante pour capteur.
+	 * @param capteurSlot le slot du capteur.
+	 */
 	public VariableCapteur(CapteurSlot capteurSlot) {
 		super(TypeVariable.INT, "", CapteurSlot.A.toString());
 		this.capteurSlot = capteurSlot;
-		
 	}
 	
-	public void setCapteur ( CapteurSlot capteurSlot){
+	/**
+	 * Modifie le slot du capteur.
+	 * @param capteurSlot le nouveau slot
+	 */
+	public void setCapteur(CapteurSlot capteurSlot) {
 		this.capteurSlot = capteurSlot;
 		this.setValeur(capteurSlot.toString());
 	}
 	
+	/**
+	 * Accède au slot du capteur.
+	 * @return le slot
+	 */
 	public CapteurSlot getCapteurSlot(){
 		return capteurSlot;
 	}
 	
+	@Override
 	public void accepte(VisiteurTraduction v) {
 		v.visiter(this);
 	}
-	
-	
-	
-	
- 
-
 }
-
-
