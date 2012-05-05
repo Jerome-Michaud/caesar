056a21511f2d8c6963416565ba2ceca5a7fbaf32
Adrien <adry.azz@laposte.net>
2012-05-05 13:14:08 +0200

    Commentaires Javadoc des classes du package zones.

--- src/vue/widget/modele/zones/ChampTexte.java
+++ src/vue/widget/modele/zones/ChampTexte.java
@@ -1,17 +1,31 @@
 package vue.widget.modele.zones;
 
 import javax.swing.JTextField;
-
+/**
+ * Cette classe représente un champ texte pariculier intégrable dans un widget.
+ * @author Vincent Besnard - Quentin Gosselin
+ */
 public class ChampTexte extends JTextField implements Zone {
-	
+	/**
+	 * Constructeur faisant appel au constructeur équivalent de la classe mère.
+	 */
 	public ChampTexte() {
 		super();
 	}
-
+	
+	/**
+	 * Récupère le texte du champ texte.
+	 * @return la valeur du champ texte.
+	 */
+	@Override
 	public String getValeur() {
 		return this.getText();
 	}
-
+	/**
+	 * Modifie le texte du champ texte.
+	 * @param v le nouveau texte du champ.
+	 */
+	@Override
 	public void setValeur(String v) {
 		this.setText(v);
 	}
