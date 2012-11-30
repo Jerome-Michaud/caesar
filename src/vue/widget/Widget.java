84d6c87bbc7f9033c853ed993d908dcb3e0012dd
Bastien <Bastien@172.18.217.68>
2012-11-30 14:57:57 +0100

    Correction de plusieurs bugs graphiques

--- src/vue/widget/Widget.java
+++ src/vue/widget/Widget.java
@@ -84,7 +84,7 @@ public class Widget extends JComponent {
 		this.setFocusable(true);
 		this.setOpaque(true);
 		this.setForme(true);
-
+                
 		initListeners();
 
 		for (Zone z : this.modele.getLesZonesSaisies()) {
