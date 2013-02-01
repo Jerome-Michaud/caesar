e0b69a01287f70b12a7fb507cb34d5ec43331930
Guillaume Delorme <guillaume.delorme@mail.com>
2013-02-01 17:04:27 +0100

    Correction widgets qui acceptaient des expressions arithmétiques au lieu de logique

--- src/jscratch/vue/widgets/modeles/IfElseWidget.java
+++ src/jscratch/vue/widgets/modeles/IfElseWidget.java
@@ -35,7 +35,7 @@ public class IfElseWidget extends ModeleWidget implements Serializable {
 		
 		ChampTexte f = new ChampTexte();
 		f.ajouterTypeWidgetAccepte(TypeModeleWidget.VARIABLE);
-		f.ajouterTypeWidgetAccepte(TypeModeleWidget.EXPRESSION_ARITHMETIQUE);
+		f.ajouterTypeWidgetAccepte(TypeModeleWidget.EXPRESSION_LOGIQUE);
 		f.setBounds(55, 3, 20, 20);
 		f.supprimerTexte();
 		this.getLesZonesSaisies().add(f);
