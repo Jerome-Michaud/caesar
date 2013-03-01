830d0debfa7b906d7f1cac25b4d4ab381bece6ab
Adrien <adry.azz@laposte.net>
2013-03-01 14:54:50 +0100

    Correction de IncrementationWidget.

--- src/jscratch/vue/widgets/modeles/IncrementationWidget.java
+++ src/jscratch/vue/widgets/modeles/IncrementationWidget.java
@@ -7,8 +7,11 @@ import java.awt.Polygon;
 import java.awt.geom.Rectangle2D;
 
 import jscratch.helpers.FontHelper;
+import jscratch.vue.widgets.Widget;
+import jscratch.vue.widgets.modeles.zones.ChampTexte;
 import nxtim.instruction.InstructionIncrementation;
 import nxtim.instruction.Operateur;
+import nxtim.instruction.VariableModifiable;
 
 /**
  * @since 1.0
@@ -19,9 +22,13 @@ public class IncrementationWidget extends ModeleWidget {
 	private int largeur;
 
 	private final int LARG_EXTREMITE;
+	private ChampTexte chmpVar;
 	
 	/**
-	 * Default constructor of <code>IncrementationPlusWidget</code>.
+	 * Créé un widget d'incrémentation.
+	 * 
+	 * @param op l'opérateur d'incrémentation ou de décrémentation.
+	 * @param isAvant <code>true</code> indique que l'opérateur est avant la variable, <code>false</code> qu'il est après.
 	 */
 	public IncrementationWidget(Operateur op, final boolean isAvant) {
 		InstructionIncrementation ins;
@@ -40,29 +47,58 @@ public class IncrementationWidget extends ModeleWidget {
 
 		//graphisme
 		this.LARG_EXTREMITE = 5;
-
-		message.put(new Point(LARG_EXTREMITE, 15), op.toString());
-
+		final int widthChmp = 14;
+		chmpVar = new ChampTexte(widthChmp, this);
+		chmpVar.supprimerTexte();
+		chmpVar.ajouterTypeWidgetAccepte(TypeModeleWidget.VARIABLE);
+		
 		Font font = FontHelper.getWidgetFont();
 		FontMetrics metrics = new FontMetrics(font) {
 		};
 		Rectangle2D bounds = metrics.getStringBounds(op.toString(), null);
-
-		this.largeur = (int) bounds.getWidth();
-		int tabX[] = {0, 5, this.LARG_EXTREMITE + largeur, largeur + 10, largeur + 10, this.LARG_EXTREMITE + largeur, 5, 0};
+		if(isAvant) {
+			message.put(new Point(LARG_EXTREMITE, 3), op.toString());
+			chmpVar.setBounds(LARG_EXTREMITE + (int) bounds.getWidth() + 5, 15, widthChmp, 14);
+		} else {
+			message.put(new Point(LARG_EXTREMITE + widthChmp + 5, 15), op.toString());
+			chmpVar.setBounds(LARG_EXTREMITE, 3, widthChmp, 14);
+		}
+		this.getLesZonesSaisies().add(chmpVar);
+		
+		this.largeur = (int) bounds.getWidth() + 5 + widthChmp;
+		int tabX[] = {0, 5, LARG_EXTREMITE + largeur, largeur + 10, largeur + 10, this.LARG_EXTREMITE + largeur, 5, 0};
 		int tabY[] = {5, 0, 0, 5, 15, 20, 20, 15};
 
 		this.setTabX(tabX);
 		this.setTabY(tabY);
-
-		this.setForme(new Polygon(this.getTabX(), this.getTabY(), this.getTabX().length));
-
 		this.setTailleX();
 		this.setTailleY();
 
+		this.setForme(new Polygon(this.getTabX(), this.getTabY(), this.getTabX().length));
+
 		initListeners();
 	}
-
+	
 	@Override
-	public void initListeners() { }
+	public void decalageX(int x) {
+		int i;
+		for (i = 2; i < this.getTabX().length - 2; i++) {
+			this.getForme().xpoints[i] = this.getForme().xpoints[i] + x;
+		}
+		this.setForme(this.getForme());
+		this.setTailleX();
+	}
+	
+	@Override
+	public void applyChangeModele() {
+		InstructionIncrementation exp = ((InstructionIncrementation) getElementProgramme());
+		
+		Widget widgetVar = chmpVar.getContentWidget();
+		if (widgetVar != null) {
+			VariableModifiable variable = (VariableModifiable) widgetVar.getElementProgramme();
+			exp.setVariable(variable);
+		} else {
+			exp.setVariable(null);
+		}
+	}
 }
