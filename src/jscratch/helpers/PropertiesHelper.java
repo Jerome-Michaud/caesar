005b6d7aa4acfdb4078cfabb5b6c2743fda17266
Adrien <ad.duroy@gmail.com>
2014-09-19 20:47:49 +0200

    Modification du mécanisme de chargement des propriétés et génération du fichier de properties.properties si absent. Le fichier properties.properties est maintenant recherché le dossier '.caesar'.

--- src/jscratch/helpers/PropertiesHelper.java
+++ src/jscratch/helpers/PropertiesHelper.java
@@ -43,17 +43,19 @@ package jscratch.helpers;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import jscratch.Environnement;
 
 /**
  * Permet de gérer facilement les fichiers properties.
@@ -62,16 +64,20 @@ import java.util.logging.Logger;
  * @version 1.0
  */
 public final class PropertiesHelper {
-
+	private static final String PROPERTIES_FILE = "properties.properties";
+	private static final String DISTANT_PROPERTIES_KEY = "user.chemin.properties.distant";
+	private static final String ENCODING = "UTF8";
+	private static final String COMMENT_FILE_PROPERTIES = "Ce fichier a été généré par CAESAR";
+	
 	/**
 	 * Unique instance of <code>PropertiesHelper</code>.
 	 */
 	private static PropertiesHelper instance = null;
 
 	/**
-	 * Les fichier properties.
+	 * Le fichier properties.
 	 */
-	private SortedProperties properties = null, propertiesDistant = null;
+	private SortedProperties properties = null;
 	
 	/**
 	 * Private constructor of <code>PropertiesHelper</code>.
@@ -79,20 +85,39 @@ public final class PropertiesHelper {
 	 * @since 1.0
 	 */
     private PropertiesHelper() {
+    	InputStreamReader reader = null;
 		try {
-			File fichier = new File("properties.properties");
-			if (fichier.exists()) {
-				this.properties = new SortedProperties();
-				this.properties.load(new InputStreamReader(new FileInputStream(fichier), "UTF8"));
-				
-				File fichierdistant = new File(this.properties.getProperty("user.chemin.properties.distant"));
-				if (fichierdistant.exists()) {
-					this.propertiesDistant = new SortedProperties();
-					this.propertiesDistant.load(new InputStreamReader(new FileInputStream(fichierdistant), "UTF8"));
-				}
+			File fichier = new File(Environnement.getDataDir(), PROPERTIES_FILE);
+			// Creer le fichier de propriétés par défaut s'il n'exista pas
+			if (!fichier.exists()) {
+				creerFichierProperties();
 			}
-		} catch (FileNotFoundException ex) {
-			ErreurHelper.afficher(ex);
+			this.properties = new SortedProperties();
+			// Initialisation avec les valeurs par défaut pour être sûr que les valeurs obligatoires soient présentes
+			setToDefaultObligatoryProperties(this.properties);
+			reader = new InputStreamReader(new FileInputStream(fichier), ENCODING);
+			this.properties.load(reader);
+			reader.close();
+			reader = null;
+			
+			String fichierProp = this.properties.getProperty(DISTANT_PROPERTIES_KEY);
+			if(fichierProp != null) {
+				File fichierdistant = new File(fichierProp);
+				// Si le chemin du fichier distant est
+				if(!fichierdistant.isAbsolute()) {
+					fichierdistant = new File(Environnement.getDataDir(), fichierProp);
+				}
+				if (fichierdistant.exists()) {
+					reader = new InputStreamReader(new FileInputStream(fichierdistant), ENCODING);
+					this.properties.clear();
+					this.properties.load(reader);
+					reader.close();
+				} else {
+					System.err.println("Fichier " + fichierdistant.getAbsolutePath() + " introuvable !");
+				}
+			} else {
+				System.err.println("Manque la propriété '" + DISTANT_PROPERTIES_KEY + "' dans le fichier " + PROPERTIES_FILE + ".");
+			}
 		} catch (IOException ex) {
 			ErreurHelper.afficher(ex);
 		}
@@ -107,11 +132,21 @@ public final class PropertiesHelper {
 	 */
 	public void reset(final File fichier) {
 		if (fichier.exists()) {
+            InputStreamReader reader = null;
 			try {
-				this.propertiesDistant = new SortedProperties();
-				this.propertiesDistant.load(new InputStreamReader(new FileInputStream(fichier), "UTF8"));
+				this.properties = new SortedProperties();
+				reader = new InputStreamReader(new FileInputStream(fichier), ENCODING);
+				this.properties.load(reader);
 			} catch (IOException ex) {
 				ErreurHelper.afficher(ex);
+			} finally {
+				if(reader != null) {
+					try {
+						reader.close();
+					} catch(IOException ioe) {
+						ErreurHelper.afficher(ioe);
+					}
+				}
 			}
 		}
 	}
@@ -123,7 +158,7 @@ public final class PropertiesHelper {
 	 * 
 	 * @return unique instance of <code>PropertiesHelper</code>
 	 */
-    public static PropertiesHelper getInstance() {
+    public static synchronized PropertiesHelper getInstance() {
         if (instance == null) {
 			instance = new PropertiesHelper();
 		}
@@ -138,7 +173,7 @@ public final class PropertiesHelper {
 	 * @return la nombre de clé
 	 */
 	public int size() {
-		return this.propertiesDistant.size();
+		return this.properties.size();
 	}
 	
 	/**
@@ -150,7 +185,7 @@ public final class PropertiesHelper {
 	 * @return la valeur de la propriété
 	 */
 	public String get(final String cle) {
-		String valeur = this.propertiesDistant.getProperty(cle);
+		String valeur = this.properties.getProperty(cle);
 		if (valeur == null) {
 			Logger.getLogger(PropertiesHelper.class.toString()).log(Level.WARNING, "La clé ''{0}'' n''a pas été trouvée", new Object[] {cle});
 		}
@@ -167,13 +202,13 @@ public final class PropertiesHelper {
 	 * @return la valeur de la propriété
 	 */
 	public String get(final String cle, final boolean ajout) {
-		String valeur = this.propertiesDistant.getProperty(cle);
+		String valeur = this.properties.getProperty(cle);
 		if (!ajout) {
 			return get(cle);
 		}
 		else if (valeur == null || valeur.isEmpty()) {
 			Logger.getLogger(PropertiesHelper.class.toString()).log(Level.WARNING, "La clé ''{0}'' n''a pas été trouvée mais ajoutée sans valeur", new Object[] {cle});
-			this.propertiesDistant.put(cle, "");
+			this.properties.put(cle, "");
 		}
 		return valeur;
 	}
@@ -187,7 +222,7 @@ public final class PropertiesHelper {
 	 * @param valeur la nouvelle valeur de cette propriété
 	 */
 	public void set(final String cle, final String valeur) {
-		this.propertiesDistant.setProperty(cle, valeur);
+		this.properties.setProperty(cle, valeur);
 	}
 	
 	/**
@@ -196,19 +231,122 @@ public final class PropertiesHelper {
 	 * @since 1.0
 	 */
 	public void sauvegarder() {
+		Writer writer = null;
+        InputStreamReader reader = null;
 		try {
-			File fichier = new File(PropertiesHelper.getInstance().get("user.chemin.properties.distant"));
-			if (!fichier.exists()) {
-				fichier.createNewFile();
+			String distantProperties = PropertiesHelper.getInstance().get(DISTANT_PROPERTIES_KEY);
+			// Le fichier utilisé est celui définit par la clé DISTANT_PROPERTIES_KEY si elle existe, sinon c'est PROPERTIES_FILE
+			File fichier = new File(Environnement.getDataDir(), PROPERTIES_FILE);
+			if(distantProperties != null) {
+				File fichierDistant = new File(distantProperties);
+				// Mise à jour du chemin du properties distant dans le properties local
+				reader = new InputStreamReader(new FileInputStream(fichier), ENCODING);
+				final SortedProperties prop = new SortedProperties();
+				prop.load(reader);
+				prop.setProperty(DISTANT_PROPERTIES_KEY, PropertiesHelper.getInstance().get(DISTANT_PROPERTIES_KEY));
+				writer = new OutputStreamWriter(new FileOutputStream(fichier), ENCODING);
+				prop.store(writer, COMMENT_FILE_PROPERTIES);
+				writer.close();
+				writer = null;
+				// Mise à jour des propriétés dans le fichier distant
+				writer = new OutputStreamWriter(new FileOutputStream(fichierDistant), ENCODING);
+				this.properties.remove(DISTANT_PROPERTIES_KEY); // Pour ne pas stocker la propriétés de chemin distant
+				this.properties.store(writer, COMMENT_FILE_PROPERTIES);
+				this.properties.setProperty(DISTANT_PROPERTIES_KEY, prop.getProperty(DISTANT_PROPERTIES_KEY));
+			} else {
+				writer = new OutputStreamWriter(new FileOutputStream(fichier), ENCODING);
+				this.properties.store(writer, COMMENT_FILE_PROPERTIES);
 			}
-			
-			// Mise à jour du chemin du properties distant dans le properties local
-			this.properties.setProperty("user.chemin.properties.distant", PropertiesHelper.getInstance().get("user.chemin.properties.distant"));
-			
-			this.propertiesDistant.store(new OutputStreamWriter(new FileOutputStream(fichier), "UTF8"), "");
-			this.properties.store(new OutputStreamWriter(new FileOutputStream(new File("properties.properties")), "UTF8"), "");
 		} catch (IOException ex) {
 			ErreurHelper.afficher(ex);
+		} finally {
+			try {
+				if(reader != null) {
+					reader.close();
+				}
+				if(writer != null) {
+					writer.close();
+				}
+			} catch(IOException e) {
+				ErreurHelper.afficher(e);
+			}
+		}
+	}
+	
+	/**
+	 * Modifie un ensemble de propriétés en celui par des propriétés obligatoires par défaut.
+	 * @param props l'ensemble de propriétés à changer
+	 */
+	private void setToDefaultObligatoryProperties(SortedProperties props) {
+		props.clear();
+		props.setProperty("taille.categorie.x", "110");
+		props.setProperty("taille.categorie.y", "25");
+		props.setProperty("marge.aiment", "10");
+		props.setProperty("linestroke.value", "5");
+		props.setProperty("taux.transfert", "0.6");
+		props.setProperty("ecart.defaut", "15");
+		props.setProperty("user.chemin.compilateur", "nbc.exe");
+		props.setProperty("user.categorie.afficher.nombrewidget", "true");
+		props.setProperty("user.couleur.Structures", "-692194");
+		props.setProperty("user.couleur.Moteurs", "-7814912");
+		props.setProperty("user.couleur.Expressions", "-692194");
+		props.setProperty("user.couleur.Capteurs", "-7062134");
+		props.setProperty("user.couleur.Temps", "-16735797");
+		props.setProperty("user.couleur.Variables", "-1487241");
+	}
+	
+	/**
+	 * Modifie un ensemble de propriétés en celui des propriétés par défaut.
+	 * @param props l'ensemble de propriétés à changer
+	 */
+	private void setToDefaultProperties(SortedProperties props) {
+		setToDefaultObligatoryProperties(props);
+		props.setProperty("user.interface.afficher.categories", "true");
+		props.setProperty("user.widget.afficher.Attendre", "true");
+		props.setProperty("user.widget.afficher.AffecterA", "true");
+		props.setProperty("user.widget.afficher.Capteur", "true");
+		props.setProperty("user.widget.afficher.ConfigurationCapteurs", "true");
+		props.setProperty("user.widget.afficher.DeclarerVariable", "true");
+		props.setProperty("user.widget.afficher.ExpressionArithmetique", "true");
+		props.setProperty("user.widget.afficher.ExpressionIncrementation", "true");
+		props.setProperty("user.widget.afficher.ExpressionLogique", "true");
+		props.setProperty("user.widget.afficher.FaireTantQue", "true");
+		props.setProperty("user.widget.afficher.MoteurArret", "true");
+		props.setProperty("user.widget.afficher.MoteurMarche", "true");
+		props.setProperty("user.widget.afficher.MoteurNombreRotation", "true");
+		props.setProperty("user.widget.afficher.MoteurRAZ", "true");
+		props.setProperty("user.widget.afficher.Repeter", "true");
+		props.setProperty("user.widget.afficher.Si", "true");
+		props.setProperty("user.widget.afficher.SiSinon", "true");
+		props.setProperty("user.widget.afficher.Tache", "true");
+		props.setProperty("user.widget.afficher.TantQue", "true");
+		props.setProperty("user.widget.afficher.TempsCourant", "true");
+		props.setProperty("user.widget.afficher.Variable", "true");
+		props.setProperty("user.widget.afficher.Negation", "true");
+	}
+	
+	/**
+	 * Créer le fichier properties par défaut.
+	 */
+	private void creerFichierProperties() {
+		File fichier = new File(Environnement.getDataDir(), PROPERTIES_FILE);
+		SortedProperties props = new SortedProperties();
+		setToDefaultProperties(props);
+
+		Writer writer = null;
+		try {
+			writer = new OutputStreamWriter(new FileOutputStream(fichier), ENCODING);
+			props.store(writer, COMMENT_FILE_PROPERTIES);
+		} catch (IOException e) {
+			ErreurHelper.afficher(e);
+		} finally {
+			if(writer != null) {
+				try {
+					writer.close();
+				} catch(IOException e) {
+					ErreurHelper.afficher(e);
+				}
+			}
 		}
 	}
 	
@@ -219,14 +357,19 @@ public final class PropertiesHelper {
 	 */
 	private class SortedProperties extends Properties {
 		
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * {@inheritDoc}
+		 */
 		@Override
-		public synchronized Enumeration keys() {
-			Enumeration keysEnum = super.keys();
-			Vector keyList = new Vector();
+		public synchronized Enumeration<Object> keys() {
+			Enumeration<Object> keysEnum = super.keys();
+			Vector<Object> keyList = new Vector<Object>();
 			while (keysEnum.hasMoreElements()) {
 				keyList.add(keysEnum.nextElement());
 			}
-			Collections.sort(keyList);
+			keyList.sort(null);
 			return keyList.elements();
 		}
 	}
