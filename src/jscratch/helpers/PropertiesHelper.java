ea82afaa51c6227f74e97f162f4038ddf7ab750e
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2014-09-29 16:56:43 +0200

    Tri des clés du fichier properties

--- src/jscratch/helpers/PropertiesHelper.java
+++ src/jscratch/helpers/PropertiesHelper.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) Université du Maine (2013) 
+Copyright (C) Université du Maine (2013)
 
 contributeurs : Adrien Duroy, Bastien Andru, Quentin Gosselin, Guillaume Delorme,
  Nicolas Detan, Zubair Parwany, Houda Chouket, Bastien Aubry,
@@ -10,12 +10,12 @@ ad.duroy@gmail.com
 Ce fichier est une partie du logiciel CAESAR.
 
 CAESAR est un programme informatique servant à construire un programme
-pour un robot NXT et à effectuer une simulation de l'exécution de ce dernier. 
+pour un robot NXT et à effectuer une simulation de l'exécution de ce dernier.
 
 CAESAR est régi par la licence CeCILL soumise au droit français et
 respectant les principes de diffusion des logiciels libres. Vous pouvez
 utiliser, modifier et/ou redistribuer ce programme sous les conditions
-de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
 sur le site "http://www.cecill.info".
 
 En contrepartie de l'accessibilité au code source et des droits de copie,
@@ -26,16 +26,16 @@ titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
 associés au chargement,  à l'utilisation,  à la modification et/ou au
-développement et à la reproduction du logiciel par l'utilisateur étant 
-donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
 manipuler et qui le réserve donc à des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
 utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
 logiciel à leurs besoins dans des conditions permettant d'assurer la
-sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
-à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL, et que vous en avez accepté les
 termes.
  */
@@ -48,6 +48,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Properties;
@@ -59,7 +60,7 @@ import jscratch.Environnement;
 
 /**
  * Permet de gérer facilement les fichiers properties.
- * 
+ *
  * @since 1.0
  * @version 1.0
  */
@@ -68,7 +69,7 @@ public final class PropertiesHelper {
 	private static final String DISTANT_PROPERTIES_KEY = "user.chemin.properties.distant";
 	private static final String ENCODING = "UTF8";
 	private static final String COMMENT_FILE_PROPERTIES = "Ce fichier a été généré par CAESAR";
-	
+
 	/**
 	 * Unique instance of <code>PropertiesHelper</code>.
 	 */
@@ -78,10 +79,10 @@ public final class PropertiesHelper {
 	 * Le fichier properties.
 	 */
 	private SortedProperties properties = null;
-	
+
 	/**
 	 * Private constructor of <code>PropertiesHelper</code>.
-	 * 
+	 *
 	 * @since 1.0
 	 */
     private PropertiesHelper() {
@@ -99,7 +100,7 @@ public final class PropertiesHelper {
 			this.properties.load(reader);
 			reader.close();
 			reader = null;
-			
+
 			String fichierProp = this.properties.getProperty(DISTANT_PROPERTIES_KEY);
 			if(fichierProp != null) {
 				File fichierdistant = new File(fichierProp);
@@ -122,12 +123,12 @@ public final class PropertiesHelper {
 			ErreurHelper.afficher(ex);
 		}
 	}
-	
+
 	/**
 	 * Permet de charger un autre properties.
-	 * 
+	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @param fichier le fichier à charger
 	 */
 	public void reset(final File fichier) {
@@ -155,7 +156,7 @@ public final class PropertiesHelper {
 	 * Accessor for <code>PropertiesHelper</code>.
 	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @return unique instance of <code>PropertiesHelper</code>
 	 */
     public static synchronized PropertiesHelper getInstance() {
@@ -164,23 +165,23 @@ public final class PropertiesHelper {
 		}
 		return instance;
     }
-	
+
 	/**
 	 * Perrmet de connaître le nombre de clés dans le properties.
-	 * 
+	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @return la nombre de clé
 	 */
 	public int size() {
 		return this.properties.size();
 	}
-	
+
 	/**
 	 * Récupère la valeur d'un propriété.
-	 * 
+	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @param cle la clé
 	 * @return la valeur de la propriété
 	 */
@@ -191,12 +192,12 @@ public final class PropertiesHelper {
 		}
 		return valeur;
 	}
-	
+
 	/**
 	 * Récupère la valeur d'un propriété. Si la clé n'est pas trouvé, elle est ajoutée si le deuxième paramètre est à <code>true</code>.
-	 * 
+	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @param cle la clé
 	 * @param ajout <code>true</code>, si la clé doit être ajoutée si elle n'est pas trouvée
 	 * @return la valeur de la propriété
@@ -212,22 +213,22 @@ public final class PropertiesHelper {
 		}
 		return valeur;
 	}
-	
+
 	/**
 	 * Change la valeur de la propriété.
-	 * 
+	 *
 	 * @since 1.0
-	 * 
+	 *
 	 * @param cle la clé de la propriété à changer
 	 * @param valeur la nouvelle valeur de cette propriété
 	 */
 	public void set(final String cle, final String valeur) {
 		this.properties.setProperty(cle, valeur);
 	}
-	
+
 	/**
 	 * Sauvegarde les modifications apportées au fichier properties.
-	 * 
+	 *
 	 * @since 1.0
 	 */
 	public void sauvegarder() {
@@ -272,7 +273,7 @@ public final class PropertiesHelper {
 			}
 		}
 	}
-	
+
 	/**
 	 * Modifie un ensemble de propriétés en celui par des propriétés obligatoires par défaut.
 	 * @param props l'ensemble de propriétés à changer
@@ -294,7 +295,7 @@ public final class PropertiesHelper {
 		props.setProperty("user.couleur.Temps", "-16735797");
 		props.setProperty("user.couleur.Variables", "-1487241");
 	}
-	
+
 	/**
 	 * Modifie un ensemble de propriétés en celui des propriétés par défaut.
 	 * @param props l'ensemble de propriétés à changer
@@ -324,7 +325,7 @@ public final class PropertiesHelper {
 		props.setProperty("user.widget.afficher.Variable", "true");
 		props.setProperty("user.widget.afficher.Negation", "true");
 	}
-	
+
 	/**
 	 * Créer le fichier properties par défaut.
 	 */
@@ -349,14 +350,14 @@ public final class PropertiesHelper {
 			}
 		}
 	}
-	
+
 	/**
 	 * Classe supplémentaire qui permet d'avoir un properties avec les clés triées par ordre alphabétique.
-	 * 
+	 *
 	 * @since 1.0
 	 */
 	private class SortedProperties extends Properties {
-		
+
 		private static final long serialVersionUID = 1L;
 
 		/**
@@ -365,12 +366,14 @@ public final class PropertiesHelper {
 		@Override
 		public synchronized Enumeration<Object> keys() {
 			Enumeration<Object> keysEnum = super.keys();
+
+			Object[] keys = super.keySet().toArray();
+			Arrays.sort(keys);
+
 			Vector<Object> keyList = new Vector<Object>();
-			while (keysEnum.hasMoreElements()) {
-				keyList.add(keysEnum.nextElement());
-			}
-			keyList.sort(null);
+			keyList.addAll(Arrays.asList(keys));
+
 			return keyList.elements();
 		}
 	}
- }
+}
