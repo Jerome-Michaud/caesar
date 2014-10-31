314d8ebc150a1b3a25f248f7e5168ec247f01508
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2014-10-31 09:54:26 +0100

    Déplacement de deux classe interfaces qui ne sont pas à leur place

--- src/jscratch/interpreteur/ObservableInterpreteur.java
+++ src/jscratch/interpreteur/ObservableInterpreteur.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) Université du Maine (2013) 
+Copyright (C) Université du Maine (2013)
 
 contributeurs : Adrien Duroy, Bastien Andru, Quentin Gosselin, Guillaume Delorme,
  Nicolas Detan, Zubair Parwany, Houda Chouket, Bastien Aubry,
@@ -10,12 +10,12 @@ ad.duroy@gmail.com
 Ce fichier est une partie du logiciel CAESAR.
 
 CAESAR est un programme informatique servant à construire un programme
-pour un robot NXT et à effectuer une simulation de l'exécution de ce dernier. 
+pour un robot NXT et à effectuer une simulation de l'exécution de ce dernier.
 
 CAESAR est régi par la licence CeCILL soumise au droit français et
 respectant les principes de diffusion des logiciels libres. Vous pouvez
 utiliser, modifier et/ou redistribuer ce programme sous les conditions
-de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
 sur le site "http://www.cecill.info".
 
 En contrepartie de l'accessibilité au code source et des droits de copie,
@@ -26,20 +26,20 @@ titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
 associés au chargement,  à l'utilisation,  à la modification et/ou au
-développement et à la reproduction du logiciel par l'utilisateur étant 
-donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
 manipuler et qui le réserve donc à des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
 utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
 logiciel à leurs besoins dans des conditions permettant d'assurer la
-sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
-à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL, et que vous en avez accepté les
 termes.
  */
-package jscratch.vue.sim;
+package jscratch.interpreteur;
 
 import java.util.List;
 
@@ -54,23 +54,23 @@ public interface ObservableInterpreteur {
 
 	/**
 	 * ajoute un observateur sur l'interpreteur
-	 * 
+	 *
 	 * @param ObserverInterpreteur
 	 */
 	public void addObserver(ObserverInterpreteur o);
-	
+
 	/**
 	 * supprimer un observateur de l'interpreteur
-	 * 
+	 *
 	 * @param ObserverInterpreteur
 	 */
 	public void deleteObserver(ObserverInterpreteur o);
-	
+
 	/**
 	 * met à jour les observateurs
 	 */
 	public void notifyObserver();
-	
+
 	/**
 	 * met à jour les observateurs avec une commande
 	 */
